2859b257546be931a098739226cd87d6ac313f2b
Ilidio <84690116+IlidioDeos@users.noreply.github.com>
2023-11-08 07:07:00 -0300

    CRUD Complete, adding delete function

--- members-front/src/app/members-details/members-details.component.ts
+++ members-front/src/app/members-details/members-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { ApiService } from './api.service';
+import { AppComponent } from '../app.component';
 
 
 @Component({
@@ -10,7 +11,7 @@ import { ApiService } from './api.service';
 })
 export class MembersDetailsComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router) { }
+  constructor(private route: ActivatedRoute, private api: ApiService, private router: Router, private appComponent: AppComponent) { }
   selectedMember = { id: '', name: '', email: '', phone: '', address: '', city: '', state: '', zipcode: '', created: '', updated: '', profile_image: '' };
   selectedId: any;
 
@@ -46,5 +47,25 @@ export class MembersDetailsComponent implements OnInit {
   newMember() {
     this.router.navigate(['new-member']);
   }
+
+  deleteMember() {
+    this.api.deleteMember(this.selectedId).subscribe(
+      data => {
+        let index: any;
+
+        this.appComponent.members.forEach((e: any, i: any) => {
+          if (e.id === this.selectedId)
+            index = i;
+          });
+
+        this.appComponent.members.splice(index, 1);
+
+      },
+      error => {
+        console.log("Error", error);
+      }
+    );
+  };
+
 
 }
